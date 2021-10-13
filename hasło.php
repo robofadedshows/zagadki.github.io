990a4159e511a3b05df116e1cc86b77f2d1812aa
robofadedshows <92461241+robofadedshows@users.noreply.github.com>
2021-10-13 20:24:06 +0200

    Delete hasło.php

--- hasło.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo password_hash('$2y$10$4kVycr7Kija5n8oKu8hom.H3hoBnU3t715y/gqa74INM8t2cAEcyi', PASSWORD_BCRYPT);
-
-
-
-<?php
-return [
-    'pass' => '$2y$10$4kVycr7Kija5n8oKu8hom.H3hoBnU3t715y/gqa74INM8t2cAEcyi'
-];
