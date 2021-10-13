5613ea9a624db9ea63b79e19472f23558a0bac1b
robofadedshows <92461241+robofadedshows@users.noreply.github.com>
2021-10-13 20:12:25 +0200

    Create hasło.php

---
+++ hasło.php
@@ -0,0 +1,9 @@
+<?php
+echo password_hash('$2y$10$4kVycr7Kija5n8oKu8hom.H3hoBnU3t715y/gqa74INM8t2cAEcyi', PASSWORD_BCRYPT);
+
+
+
+<?php
+return [
+    'pass' => '$2y$10$4kVycr7Kija5n8oKu8hom.H3hoBnU3t715y/gqa74INM8t2cAEcyi'
+];
